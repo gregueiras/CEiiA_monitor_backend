0a5f9843a658c9a345d002149e833c1b2a1cebe4
Gonçalo Santos <gregueiras@gmail.com>
2019-08-08 14:53:06 +0100

    selective socket mvp

---
+++ src/fakeData.js
@@ -0,0 +1,65 @@
+const readline = require("readline")
+
+function inputHandler(keyPressed, sendData, clients) {
+  let location, data
+  switch (keyPressed) {
+    case "q": {
+      location = "S. Miguel"
+      data = 8
+      break
+    }
+    case "a": {
+      location = "S. Miguel"
+      data = 5
+      break
+    }
+    case "z": {
+      location = "S. Miguel"
+      data = 2
+      break
+    }
+    case "w": {
+      location = "Horta"
+      data = 8
+      break
+    }
+    case "s": {
+      location = "Horta"
+      data = 5
+      break
+    }
+    case "x": {
+      location = "Horta"
+      data = 2
+      break
+    }
+    case "c": {
+      process.exit()
+    }
+      
+    case "d": {
+      console.log()
+    }
+    default:
+      break
+  }
+  if (location && data) {
+    console.log(location, data)
+    const receivingClients = clients[location]
+    receivingClients.forEach(client => sendData(client, location, data))
+  }
+}
+
+function fakeDataSetup(locations, sendData, clients) {
+  locations.push("S. Miguel")
+  locations.push("Horta")
+
+  readline.emitKeypressEvents(process.stdin)
+  process.stdin.setRawMode(true)
+
+  process.stdin.on("keypress", keyPressed => inputHandler(keyPressed, sendData, clients))
+}
+
+module.exports = {
+  fakeDataSetup,
+}
---
+++ src/index.js
@@ -0,0 +1,46 @@
+const io = require("socket.io")()
+const md5 = require("md5")
+const { fakeDataSetup } = require("./fakeData")
+
+const clients = {}
+const port = 3333
+const locations = []
+
+fakeDataSetup(locations, sendData, clients)
+console.log(locations)
+setup()
+
+io.listen(port)
+console.log("listening on port ", port)
+
+function setup() {
+  io.on("connection", client => {
+    locations.forEach(location => handleConnection(client, location))
+  })
+}
+
+function getSubscriptionName(name) {
+  return `update${md5(name)}`
+}
+
+function sendData(client, location, value) {
+  client && location && client.emit(getSubscriptionName(location), value)
+}
+
+function handleConnection(client, location) {
+  const subName = getSubscriptionName(location)
+  console.log(`Server listening to "${subName}"`)
+  client.on(subName, () => {
+    console.log(`client is subscribing to updates from ${location}`)
+    addClient(location, client)
+  })
+}
+
+function addClient(location, client) {
+  if (Object.keys(clients).includes(location)) {
+    clients[location].push(client)
+  } else {
+    clients[location] = [client]
+  }
+  console.log(clients)
+}
